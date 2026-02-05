90a800764ce9a2582cc5cc68cab306228210b160
Yuri Santos <yuri.rps@gmail.com>
2026-02-04 22:25:00 -0300

    #5 Introducing Signals

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -14,5 +14,19 @@ export class Hello {
   protected onClick() {
     console.log('button clicked');
     this.isDisabled = !this.isDisabled;
+  }
+
+  protected count = signal(0);
+
+  increateCounter() {
+    this.count.update( value => value + 1)
+  }
+
+  decreaseCounter() {
+    this.count.update(value => value - 1)
+  }
+
+  resetCounter() {
+    this.count.set(0)
   }
 }
